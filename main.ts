8b642360ff856cf65e9ebd7697b9c5cd4b8d3186
Ari-0717 <s124176@hys.edu.hk>
2025-05-16 15:44:55 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendString("Intruder")
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+let pressure_sensor = 0
+basic.forever(function () {
+	
+})
